3d010274c6fab7ca2fa5b96a3416e37d162f7af1
Dmytro <voronok16@gmail.com>
2019-09-23 20:58:06 +0300

    Updates

---
+++ precache-manifest.d7949a0588e1966ebf6acc473f08550b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d370dfe03ebb8eb4913a27828e3e0192",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "91fda969c1a70f691244",
+    "url": "/react_dynamic-list-of-todos/static/css/main.58ae7290.chunk.css"
+  },
+  {
+    "revision": "6b65a40088deff272b22",
+    "url": "/react_dynamic-list-of-todos/static/js/2.b0c2f2c9.chunk.js"
+  },
+  {
+    "revision": "91fda969c1a70f691244",
+    "url": "/react_dynamic-list-of-todos/static/js/main.fc392bac.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
